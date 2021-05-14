31a9332ac51d2b095dfb379b4be59afca525233a
v2rgo91 <yejin100466@naver.com>
2021-05-14 14:46:18 +0900

    filter 비활성화

--- pybo/filters.py
+++ pybo/filters.py
@@ -1,10 +1,10 @@
-# import babel.
-
-def format_datetime(value, fmt='%Y년 %m월 %H:%M'):
-    return babel.dates.format_datetime(value,fmt)
-
-def format_str(value, fmt='%s'):
-    return babel.str.format_str(value,fmt)
-
-
-
+# # import babel.
+#
+# def format_datetime(value, fmt='%Y년 %m월 %H:%M'):
+#     return babel.dates.format_datetime(value,fmt)
+#
+# def format_str(value, fmt='%s'):
+#     return babel.str.format_str(value,fmt)
+#
+#
+#
